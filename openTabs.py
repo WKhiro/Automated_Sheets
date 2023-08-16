ef12cd7691d10d8e72b4609f7e3981d9ff3de4aa
Wesley Kok <36036408+WKhiro@users.noreply.github.com>
2023-08-16 16:03:08 -0700

    Click an extra button for me

--- openTabs.py
+++ openTabs.py
@@ -24,10 +24,10 @@ scopes = [
 ]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "INSERT CREDS FILE PATH", scopes=scopes
+    "INSERT PATH TO LOCAL CREDS FILE", scopes=scopes
 )
 file = gspread.authorize(creds)
-workbook = file.open("INSERT SPREADSHEET NAME")
+workbook = file.open("INSERT GOOGLE SHEET NAME")
 sheet = workbook.sheet1
 
 driver.get("INSERT LINK")
@@ -63,12 +63,28 @@ for row in reversed(rows):
         break
     else:
         element = row.find_element(By.TAG_NAME, "a")
-        ActionChains(driver).move_to_element(element).perform()
+        action.move_to_element(element).perform()
         action.key_down(Keys.CONTROL).click(element).key_up(Keys.CONTROL).perform()
 
 # Gather the other tab handles before closing the current tab
 otherTabs = driver.window_handles
 driver.close()
+
+
+def clickAndWaitForSubmission(xpath):
+    buttonElement = driver.find_element(By.XPATH, xpath)
+    action.move_to_element(buttonElement).perform()
+    action.click(buttonElement).perform()
+    # Wait for the next page to load, otherwise the click won't go through
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                "//table[@class='table table-striped table-bordered table-hover table-condensed ng-scope']",
+            )
+        )
+    )
+    driver.close()
 
 
 def updateSheet(row, applicant, result, reason=None):
@@ -80,7 +96,25 @@ def updateSheet(row, applicant, result, reason=None):
         sheet.update_cell(i, 3, reason)
 
 
-keyList = ["1", "2", "3", "4", "5", "6", "q", "e", "r", "a", "s", "d", "f", "x", "c"]
+keyList = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "q",
+    "e",
+    "r",
+    "a",
+    "s",
+    "d",
+    "f",
+    "x",
+    "c",
+    "v",
+    "l",
+]
 
 for i in range(1, tabs):
     driver.switch_to.window(otherTabs[i])
@@ -99,36 +133,60 @@ for i in range(1, tabs):
         key = keyboard.read_key()
     if key == "1":
         updateSheet(i, applicant, "APPROVE")
+        clickAndWaitForSubmission("//i[@class='icon icon-ok']")
     elif key == "2":
-        updateSheet(i, applicant, "REJECT")
+        updateSheet(i, applicant, "REJECT", "Already reset twice")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
     elif key == "3":
         updateSheet(i, applicant, "RESET", "VIDEO: Frozen recording")
+        clickAndWaitForSubmission("//i[@class='icon icon-facetime-video']")
     elif key == "4":
         updateSheet(i, applicant, "RESET", "VIDEO: Sideways recording")
+        clickAndWaitForSubmission("//i[@class='icon icon-facetime-video']")
     elif key == "5":
         updateSheet(i, applicant, "RESET", "VIDEO: Black screen")
+        clickAndWaitForSubmission("//i[@class='icon icon-facetime-video']")
     elif key == "6":
         updateSheet(i, applicant, "RESET", "VIDEO: Wrong screen")
+        clickAndWaitForSubmission("//i[@class='icon icon-facetime-video']")
     elif key == "q":
         updateSheet(i, applicant, "RESET", "PERFORMANCE: NoArticle")
+        clickAndWaitForSubmission("//i[@class='icon icon-user']")
     elif key == "e":
         updateSheet(i, applicant, "RESET", "PERFORMANCE: Bkgrd Noise")
+        clickAndWaitForSubmission("//i[@class='icon icon-user']")
     elif key == "r":
         updateSheet(i, applicant, "RESET", "PERFORMANCE: MC")
+        clickAndWaitForSubmission("//i[@class='icon icon-user']")
     elif key == "a":
         updateSheet(i, applicant, "RESET", "AUDIO: NoVerbal")
+        clickAndWaitForSubmission("//i[@class='icon icon-volume-up']")
     elif key == "s":
         updateSheet(i, applicant, "RESET", "AUDIO: PoorAudio")
+        clickAndWaitForSubmission("//i[@class='icon icon-volume-up']")
     elif key == "d":
         updateSheet(i, applicant, "REJECT", "Already reset once; no article")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
     elif key == "f":
         updateSheet(i, applicant, "REJECT", "Already reset once; background noise")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
     elif key == "x":
         updateSheet(i, applicant, "REJECT", "Already reset once; wrong mc")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
     elif key == "c":
         updateSheet(i, applicant, "REJECT", "Already reset once; poor audio")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
+    elif key == "v":
+        updateSheet(i, applicant, "REJECT", "Already reset once; recording issue")
+        clickAndWaitForSubmission("//i[@class='icon icon-remove']")
+    elif key == "l":
+        driver.quit()
+        break
     key = keyboard.read_key()
     while key != "w":
+        if key == "l":
+            driver.quit()
+            break
         print("Rechecking for closed window")
         key = keyboard.read_key()
     driver.close()
